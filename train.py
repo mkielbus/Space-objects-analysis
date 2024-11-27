33827c4e57b1285917b22032dd9d6800431b850b
Marcin <01169256@pw.edu.pl>
2024-11-27 11:54:44 +0100

    intial commit

---
+++ train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+
+def main() -> None:
+    model = YOLO("yolov8n.pt")
+    model.train(data="data\\config.yaml", epochs=1, project="D:\\studia\\FO\\runs")
+    # results = model.val()  # evaluate model performance on the validation set
+    # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+    # success = model.export(format="onnx")  # export the model to ONNX format
+
+
+if __name__ == "__main__":
+    main()
