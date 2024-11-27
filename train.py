f9718c7b446852cc706f37c99da25b5f926266c3
Marcin <01169256@pw.edu.pl>
2024-11-27 13:53:14 +0100

    add instructions to readme

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 def main() -> None:
     model = YOLO("yolov8n.pt")
-    model.train(data="data\\config.yaml", epochs=1, project="D:\\studia\\FO\\runs")
+    model.train(data="data\\config.yaml", epochs=1, project="runs")
     # results = model.val()  # evaluate model performance on the validation set
     # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
     # success = model.export(format="onnx")  # export the model to ONNX format
